502cb4e26371c7210ae1463f105b22f0f80a7f85
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-24 16:19:12 -0500

    Initial Commit

---
+++ ReduxCore/ReduxCore.h
@@ -0,0 +1,18 @@
+//
+//  ReduxCore.h
+//  ReduxCore
+//
+//  Created by Rone Shender on 24/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReduxCore.
+FOUNDATION_EXPORT double ReduxCoreVersionNumber;
+
+//! Project version string for ReduxCore.
+FOUNDATION_EXPORT const unsigned char ReduxCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReduxCore/PublicHeader.h>
+
+
